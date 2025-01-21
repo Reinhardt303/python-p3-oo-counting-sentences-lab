325dd594211fecb5a5cf69fdaf83b2c2dbe554c8
Thomas <thomasrains01@gmail.com>
2025-01-20 21:28:31 -0800

    submts lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,41 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+
+  def __init__(self, value = ""):
+    self.value = value
+
+  @property
+  def value(self):
+    return self._value 
+  
+  @value.setter
+  def value(self, value):
+    if type(value) == str:
+      self._value = value
+    else:
+      print("The value must be a string.")
+
+  def is_sentence(self):
+      if self.value.endswith("."):
+        return True
+      else:
+        return False
+
+  def is_question(self):
+    if self.value.endswith("?"):
+      return True
+    else:
+      return False
+    
+  def is_exclamation(self):
+    if self.value.endswith("!"):
+      return True
+    else:
+      return False
+
+#test works with proper punctuation, but test uses teenager english
+  def count_sentences(self):
+    letters = [letter for letter in self.value if letter in [".","?","!"]]
+    return len(letters) 
+
